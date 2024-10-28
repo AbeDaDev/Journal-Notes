11bdc062a52a01d96e5c701ed507d9b13839e60c
AbeofSpade <R10NightWing@gmail.com>
2024-10-28 01:34:53 -0700

    JS Loops, functions && variables
    Took some time to have a refresher on loops. Worked on for loops and while loops. Then worked on functions and variable scopes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// ---------------------------------------------------------------- Variables
+// ---------------------------------------------------------------- Variables ----------------------------------------------------------------
 
 let myName = "Abe";
 let myLastName = "Perez";
@@ -11,7 +11,7 @@ document.getElementById("p3").textContent = `I live in ${myTown}`;
 
 
 console.log(`Hello I am ${myName} ${myLastName}`); 
-// ---------------------------------------------------------------- if statements
+// ---------------------------------------------------------------- if statements ----------------------------------------------------------------
 
 let thisAge = 25; 
 
@@ -38,12 +38,12 @@ let age = 25
 let message = age >= 18 ? "You're an adult" : "You are underage";
 
 
-// ---------------------------------------------------------------- switches 
+// ---------------------------------------------------------------- switches ----------------------------------------------------------------
 
 let day = 6
 
 switch(day) {
-     case 1: 
+     case 1:
           console.log("Today is Monday");
           break; // the reason for the break is so if a case it's true it doesn't console.log everything after it. 
      case 2: 
@@ -80,7 +80,7 @@ switch(true) {
           break;
      case testScore >= 80:
           letterGrade = "B";
-          break;
+          break
      case testScore >= 70:
           letterGrade = "C";
           break;
@@ -91,4 +91,121 @@ switch(true) {
           letterGrade = "F";
 }
 
-console.log(testScore = letterGrade)
+console.log(testScore = letterGrade);
+// ---------------------------------------------------------------- index slicing ----------------------------------------------------------------
+
+const email = "Bro1@gmail.com";
+
+let username = email.slice(0, email.indexOf("@"));
+let extension = email.slice(email.indexOf("@") + 1);
+
+console.log(username); 
+console.log(extension); 
+
+// ---------------------------------------------------------------- method chaining ----------------------------------------------------------------
+
+// let userName = window.prompt("Enter your userName address"); 
+
+userName = userName.trim();
+let letter = userName.charAt(0);
+letter = letter.toUpperCase(); 
+
+let extraChars = userName.slice(1);
+extraChars = extraChars.toLowerCase();
+userName = letter + extraChars; 
+
+console.log(userName); 
+
+// OR 
+
+let userName = window.prompt("Enter your userName");
+
+userName = userName.trim().charAt(0).toUpperCase() + userName.trim().slice(1).toLowerCase()
+
+console.log(userName);
+
+// ---------------------------------------------------------------- strict equality ----------------------------------------------------------------
+
+// an = sign is an assignment operator 
+// a == sign is a comparison operator (compare if values are equal)
+// if its a === sign then it's a strict equality operator (compare if values & datatypes are equal)
+
+const PI = 3.14
+
+if(PI === "3.14") { // if using a === sign it will not match because one is a string. == doesn't care about the data type 
+     console.log("That is PI");
+}
+else {
+     console.log("That is not PI");
+}
+
+// ---------------------------------------------------------------- while loops ----------------------------------------------------------------
+
+let username = "";
+
+while(username === "" || username === null) { // while loop will continue to execute forever while it's true 
+     username = window.prompt(`Enter your name`); // once a name was enter the condition would be false and the loop will not execute 
+}
+
+// OR
+
+let username;
+
+do { // will execute this once. Do the code first then check the condition 
+     username = window.prompt(`Enter your name`);
+} while(username === "" || username === null)
+
+     console.log(`Hello ${username}`);
+
+
+let loggedIn = false;
+let username;
+let password;
+
+while(!loggedIn) { // you can add this condition to the end and replace it with do{ function 
+     username = window.prompt("Enter your username");
+     password = window.prompt("Enter your password");
+
+     if(username === "myUsername" && password === "myPassword") {
+          loggedIn = true;
+          console.log("Logged in successfully");
+     } else {
+          console.log("Invalid credentials. Please try again.");
+}
+
+//---------------------------------------------------------------- for loops ----------------------------------------------------------------
+
+for(let i = 0; i <= 2; i ++) { // i++ increments by 1 but i+=2 increments by 2
+     console.log("hello world!") // this will log to the console 3 times. If you replace it with i it will log 0,1,2
+} // you can add a break; to break out of loops
+
+ // ---------------------------------------------------------------- functions ----------------------------------------------------------------
+
+ function happyBirthday(username, age) { // for the arguments to be passed we added username and age 
+     console.log("Happy Birthday to you!");
+     console.log("Happy Birthday to you!");
+     console.log("Happy Birthday to you!");
+     console.log(`Happy Birthday ${username} you are ${age}`); // turned this line into a template literal
+ }
+
+ happyBirthday("Abe", 35); // calling the function and sent arguments when calling it 
+
+ // ---------------------------------------------------------------- variable scopes ----------------------------------------------------------------
+
+ // let x = 3; if we add this variable, this one is global. Thi can access any function or variable that is declared. function 2 x will equal 3 but we have to remove their variables for it to be declared 
+
+function2();
+
+function function1() {
+     let x = 1;
+     console.log(x);
+}
+function function2() {
+     let x = 2; // if x = 1 was renamed to y = 1 and we try calling it from function 2 it wouldn't work because this function cant see outside of its function 
+     console.log(x);
+} 
+
+// scope means you can have to variables called x and have different values as long as they are in different scopes or functions 
+
+// ---------------------------------------------------------------- arrays ---------------------------------------------------------------- 
+
